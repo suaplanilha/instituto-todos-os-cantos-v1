bd50059ad4dd76c22a77ba5e03462a1c25dcc14f
Sua Planilha <80590317+suaplanilha@users.noreply.github.com>
2025-01-04 18:59:08 -0300

    fase1

--- js/code.js
+++ js/code.js
@@ -1,33 +1,35 @@
-/** 
+/**
  * Função que lida com requisições GET e retorna o index.html
  */
 function doGet(e) {
-
   // Obter a URL do serviço do WebApp
- var url = ScriptApp.getService().getUrl();
+  var url = ScriptApp.getService().getUrl();
 
- // Adiciona os parâmetros de largura e altura desejados
- url += "?width=100%&height=100%";
- 
- return HtmlService.createTemplateFromFile('index1').evaluate()
-   .setTitle('Controle de Alunos')
-   .setSandboxMode(HtmlService.SandboxMode.IFRAME);
+  // Adiciona os parâmetros de largura e altura desejados
+  url += "?width=100%&height=100%";
+  
+  // Retorna o HTML do index1 com o título e modo de segurança
+  return HtmlService.createTemplateFromFile('index1').evaluate()
+    .setTitle('Controle de Alunos')
+    .setSandboxMode(HtmlService.SandboxMode.IFRAME);
 }
 
 /**
-* Função para incluir outros arquivos HTML no index.html
-*/
+ * Função para incluir outros arquivos HTML no index.html
+ */
 function include(filename) {
- try {
-   return HtmlService.createHtmlOutputFromFile(filename).getContent();
- } catch (e) {
-   console.error('Erro ao incluir o arquivo:', filename, e);
-   return `<!-- Erro ao incluir o arquivo: ${filename} -->`;
- }
+  try {
+    return HtmlService.createHtmlOutputFromFile(filename).getContent();
+  } catch (e) {
+    console.error('Erro ao incluir o arquivo:', filename, e);
+    return `<!-- Erro ao incluir o arquivo: ${filename} -->`;
+  }
 }
 
-
+/**
+ * Função para obter o conteúdo de um arquivo do Google Drive
+ */
 function getFileContent(fileId) {
- var file = DriveApp.getFileById(fileId);
- return file.getBlob().getDataAsString();
+  var file = DriveApp.getFileById(fileId);
+  return file.getBlob().getDataAsString();
 }
